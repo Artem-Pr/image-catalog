4891b5c417ef2a4200d46fc8a3f8d58ea6c2be45
Artem-Pr <pryadkin1987@gmail.com>
2020-03-14 21:12:42 +0300

    Updates

---
+++ precache-manifest.3437441b032325a954b654644cca0297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453148eac7d30cf431d13b0d36a86908",
+    "url": "/image-catalog/index.html"
+  },
+  {
+    "revision": "645baac36a941d0f5e1c",
+    "url": "/image-catalog/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "02891fa6431389689fd8",
+    "url": "/image-catalog/static/css/main.e6862a4c.chunk.css"
+  },
+  {
+    "revision": "645baac36a941d0f5e1c",
+    "url": "/image-catalog/static/js/2.c2201fbf.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/image-catalog/static/js/2.c2201fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02891fa6431389689fd8",
+    "url": "/image-catalog/static/js/main.2ed87e61.chunk.js"
+  },
+  {
+    "revision": "815ca14f8e46b533fd3d",
+    "url": "/image-catalog/static/js/runtime-main.fc6a978e.js"
+  }
+]);
